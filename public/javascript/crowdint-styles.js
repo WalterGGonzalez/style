9480e1b02c9852d284f78e903d74a3baec42059c
Edmundo Perez <edmundo.perez@crowdint.com>
2015-04-01 12:18:55 -0600

    Refactors javascript for input

--- public/javascript/crowdint-styles.js
+++ public/javascript/crowdint-styles.js
@@ -1,11 +1,8 @@
 $(function() {
   // This function checks if input fields has information captured.
   // if so, it applies a class that changes the looknfeel of the input box.
-  $('input').keyup(function(){
-    if( $(this).val() != ""){
-      $(this).addClass('has-info');
-    }else{
-      $(this).removeClass('has-info');
-    }
+  $('input').keyup(function() {
+    hasInfo = $(this).val() != "";
+    $(this).toggleClass('has-info', hasInfo);
   });
 });
