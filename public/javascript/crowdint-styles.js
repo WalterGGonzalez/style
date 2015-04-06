97d2d2af30f2de2ce79b70b7363f6eced3a224c1
Edmundo Perez <edmundo.perez@crowdint.com>
2015-04-06 13:29:29 -0500

    Reset outline, implement focus behavior

--- public/javascript/crowdint-styles.js
+++ public/javascript/crowdint-styles.js
@@ -1,12 +1,6 @@
 $(function() {
   // This function checks if input fields has information captured.
   // if so, it applies a class that changes the looknfeel of the input box.
-  $('input').keyup(function() {
-    $(this).removeClass('has-info');
-    hasInfo = $(this).val() != "";
-    $(this).toggleClass('entering-info', hasInfo);
-  });
-
   $('input').focusout(function() {
     hasInfo = $(this).val() != "";
     $(this).toggleClass('has-info', hasInfo);
