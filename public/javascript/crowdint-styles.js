a49e020c4c65b1155b5721ffe47762f693a1741a
Eduardo Figarola <eduardo.figarola@crowdint.com>
2015-06-08 19:59:10 -0500

    Remove old code

--- public/javascript/crowdint-styles.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // This function checks if input fields has information captured.
-  // if so, it applies a class that changes the looknfeel of the input box.
-  $('input').focusout(function() {
-    hasInfo = $(this).val() != "";
-    $(this).toggleClass('has-info', hasInfo);
-  });
-
-});
