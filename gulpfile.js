3391362c44c73b6b4070bc8b81c97e9f97c7f7ee
Eduardo Figarola <eduardo.figarola@crowdint.com>
2015-06-08 21:24:06 -0500

    Gulp task to process sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-});
-
 gulp.task('sass', function () {
-  gulp.src('./src/css/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css/dev'));
+  gulp.src('src/css/main.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('src/css/dev'));
 });
